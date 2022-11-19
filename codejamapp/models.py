cb433230889bb0b28d2c46c55b2c88126674ec95
Jeff Zhang <zhangjeff2014@gmail.com>
2022-11-19 08:30:15 -0500

    Add events model

--- codejamapp/models.py
+++ codejamapp/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from codejamapp.database import Base
 
 
@@ -8,6 +9,20 @@ class User(Base):
     email = Column(String(50), unique = True, nullable = False)
     password = Column(String(256), nullable = False)
 
+    events = relationship("Event", back_populates="creator")
+
     def __init__(self, email: str, password: str):
         self.email = email
         self.password = password
+
+class Event(Base):
+    __tablename__ = "events"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    description = Column(Text)
+    location = Column(String(256))
+    start_time = Column(DateTime(timezone=True))
+    end_time = Column(DateTime(timezone=True))
+
+    creator_id = Column(Integer, ForeignKey("users.id"))
+    creator = relationship("User", back_populates="events")

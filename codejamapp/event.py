8edb2675e15c8c0b8fcbb8bd8c25873eba3b2e91
Constance <79022298+knstnce@users.noreply.github.com>
2022-11-19 18:46:11 -0500

    error handling

--- codejamapp/event.py
+++ codejamapp/event.py
@@ -1,5 +1,5 @@
 from flask import (Blueprint, request, url_for, redirect, render_template, flash, g, abort)
-from codejamapp.models import Event, User
+from codejamapp.models import Event
 from codejamapp import auth
 from codejamapp.database import db_session
 from sqlalchemy import exc
@@ -36,29 +36,17 @@ def edit(id):
         description = request.form["description"]
         location = request.form["location"]
         start_time = request.form["start_time"]
-        error = None
-
-        if not name:
-            error = "A name is required."
-        elif not location:
-            error = "A location is required"
-        elif not start_time:
-            error = "A start time is required"
-        elif not description:
-            error = "A description is required"
         
         start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M")
 
-        if error is None:
-            setattr(event, "name", name)
-            setattr(event, "description", description)
-            setattr(event, "location", location)
-            setattr(event, "start_time", start_time)
-            db_session.commit()
-
-        flash(error)
+        setattr(event, "name", name)
+        setattr(event, "description", description)
+        setattr(event, "location", location)
+        setattr(event, "start_time", start_time)
+        db_session.commit()
 
         return redirect(url_for("event.info", id=event.id))
+
     return render_template("event/edit.html", event=event)
 @bp.route("/create", methods=("GET", "POST"))
 @auth.login_required
@@ -68,31 +56,20 @@ def create():
         location = request.form['location']
         start_time = request.form['start_time']
         description = request.form['description']
-        error = None
 
-        if not name:
-            error = "A name is required."
-        elif not location:
-            error = "A location is required"
-        elif not start_time:
-            error = "A start time is required"
-        elif not description:
-            error = "A description is required"
-        
         start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M")
 
-        if error is None:
-            try:
-                event = Event(name, g.user.id, description, location, start_time)
-                db_session.add(event)
-                db_session.commit()
-            except exc.IntegrityError:
-                db_session.rollback()
-                error = f"An event with those details already exists!"
-            else:
-                return redirect(url_for("event.info", id=event.id))
+        try:
+            event = Event(name, g.user.id, description, location, start_time)
+            db_session.add(event)
+            db_session.commit()
+        except exc.IntegrityError:
+            db_session.rollback()
+            error = f"An event with those details already exists!"
+        else:
+            return redirect(url_for("event.info", id=event.id))
 
-            flash(error)
+        flash(error)
 
     return render_template('event/create.html')
 

a88dba56b5b57e07c63d356a528ffcd0bc45e844
Jeff Zhang <zhangjeff2014@gmail.com>
2022-11-19 16:10:55 -0500

    Show events in calendar

--- codejamapp/event.py
+++ codejamapp/event.py
@@ -3,9 +3,21 @@ from codejamapp.models import Event, User
 from codejamapp import auth
 from codejamapp.database import db_session
 from sqlalchemy import exc
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import List
 
-bp = Blueprint('event', __name__, url_prefix="/event")
+bp = Blueprint('event', __name__)
+
+@bp.route('/')
+def index():
+    now = datetime.now()
+    
+    offset = now.isoweekday() % 7 # Sunday = 0, Monday = 1, ... Saturday = 6
+    events = Event.query.filter(Event.start_time < now + timedelta(days=14-offset)) \
+                        .filter(Event.start_time > now - timedelta(days=offset)) \
+                        .order_by(Event.start_time).all()
+    events = group_by_day(events, now - timedelta(days=offset))
+    return render_template('event/index.html', events=events, offset=offset, current_day=now.day)
 
 @bp.route("/<int:id>/info", methods=("GET",))
 def info(id):
@@ -84,5 +96,32 @@ def create():
             flash(error)
 
     return render_template('event/create.html')
+
+def group_by_day(events: List[Event], start_day):
+    """Takes a list of events ordered by start time, and groups them by day."""
+    if not events:
+        return []
+
+    grouped = []
+
+    current_day = start_day
+    current_day_events = []
+    for event in events:
+        # Event on same day
+        if (current_day.year == event.start_time.year 
+            and current_day.month == event.start_time.month 
+            and current_day.day == event.start_time.day):
+            current_day_events.append(event)
+        else:
+            # Event on new day
+            grouped.append(current_day_events)
+            diff = event.start_time.day - current_day.day
+            for i in range(diff - 1):
+                grouped.append([])
+            current_day = event.start_time
+            current_day_events = [event]
+    grouped.append(current_day_events)
+    grouped += [[]] * (14 - len(grouped))
+    return grouped
 
 

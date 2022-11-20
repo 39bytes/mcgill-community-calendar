ddb0d70f9dcf75a0d6a43b49c0503a3f6fd279c7
Constance <79022298+knstnce@users.noreply.github.com>
2022-11-19 21:11:59 -0500

    you can now register two users with the same name as long as the email addresses are different

--- codejamapp/models.py
+++ codejamapp/models.py
@@ -7,7 +7,7 @@ from codejamapp.database import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    name = Column(String(100), unique=True, nullable=False)
+    name = Column(String(100), nullable=False)
     email = Column(String(50), unique = True, nullable = False)
     password = Column(String(256), nullable = False)
 
